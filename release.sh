8609369b433a63e99742b60b1685d930aa44fa2b
Robert Hailey <git@osndok.com>
2010-09-20 15:00:35 -0500

    v: pre-tag

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@
 # new release (by either a branch or tag). Many advanced git users
 # may find this script totally unnecessary.
 #
-SCRIPT_VERSION="1.0"
+SCRIPT_VERSION="1.0.0"
 # For updates or examples please visit: http://www.osndok.com/git-release/
 #
 # CHANGELOG:
