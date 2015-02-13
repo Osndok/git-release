62aa3cf38972a324c89ea19e88e0d4fbc11fdae8
Robert Hailey <rhailey@allogy.com>
2015-02-13 00:05:21 -0600

    v1.3.4

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@
 # new release (by either a branch or tag). Many advanced git users
 # may find this script totally unnecessary.
 #
-SCRIPT_VERSION="1.3.2"
+SCRIPT_VERSION="1.3.4"
 # For updates or examples please visit: http://www.osndok.com/git-release/
 #
 # BUGS:
